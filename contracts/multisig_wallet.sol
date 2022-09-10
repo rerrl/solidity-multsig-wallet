299339abc8309ab76766ac6bd5c8fc3d829ce69a
rerrl <rerrl@zzyzxlabs.xyz>
2022-09-09 20:09:05 -0700

    adds multisig wallet with tests

--- contracts/multisig_wallet.sol
+++ contracts/multisig_wallet.sol
@@ -5,7 +5,6 @@ pragma solidity ^0.8.9;
 import "hardhat/console.sol";
 /*
   2/3 Multisig
-  owner does initial wallet set up AFTER deploying the contract
   active txn array so multiple txns can be awaiting signature
 */
 
@@ -13,6 +12,37 @@ contract MultisigWallet {
     address public owner;
     uint8 immutable public signers_required;
     uint8 immutable public max_wallets;
+    address[] private validSigners;
+
+    struct Transaction {
+      address created_by;
+      address to;
+      uint256 amount_wei;
+      uint256 signatures;
+      mapping(address => bool) hasSigned;
+    }
+
+
+    mapping(uint256 => Transaction) public transactionId_to_transaction;
+    uint256 public transactionId;
+    uint256[] public pendingTransactions;
+
+    modifier onlyOwner(){
+      require(msg.sender == owner, "Only the owner can access this function.");
+      _;
+    }
+
+    modifier onlyValidSigner() {
+      bool isValid = false;
+      for(uint256 i = 0; i < validSigners.length; i++) {
+        if(msg.sender == validSigners[i]){
+          isValid = true;
+          break;
+        }
+      }
+      require(isValid, "The current address is not in the validSigners array");
+      _;
+    }
 
     constructor(uint8 _signers_required, uint8 _max_wallets){
       require(_signers_required > 0 && _max_wallets >= _signers_required, "This multisig wallet cannot be created with the desired configuration.");
@@ -20,4 +50,44 @@ contract MultisigWallet {
       signers_required = _signers_required;
       max_wallets = _max_wallets;
     }
+
+    function addSigner(address _address) onlyOwner external {
+      require(validSigners.length < max_wallets, "Adding another wallet will exceed max wallet amount.");
+
+      for(uint256 i = 0; i < validSigners.length; i++){
+        require(validSigners[i] != _address, "This address is already a valid signer!");
+      }
+      validSigners.push(_address);
+    }
+
+    // any valid signer can create a transaction
+    function createTransaction(address _to, uint256 _amount_wei) onlyValidSigner external {
+      require(address(this).balance >= _amount_wei, "Not enough Ether in this account to create transaction.");
+      Transaction storage txn = transactionId_to_transaction[transactionId];
+      txn.created_by = msg.sender;
+      txn.to = _to;
+      txn.amount_wei = _amount_wei;
+      txn.signatures = 0;
+      pendingTransactions.push(transactionId);
+      transactionId++;
+    }
+
+    function signTransaction(uint256 _txn_id) onlyValidSigner external {
+      Transaction storage txn = transactionId_to_transaction[_txn_id];
+      require(txn.hasSigned[msg.sender] == false, "This signer has already signed this transaction!");
+      txn.hasSigned[msg.sender] = true;
+      txn.signatures++;
+      console.log("Transaction %s signed by: %s", _txn_id, msg.sender);
+      if(txn.signatures == signers_required){
+        processTransaction(_txn_id);
+      }
+    }
+
+    function processTransaction(uint256 _txn_id) private {
+      Transaction storage txn = transactionId_to_transaction[_txn_id];
+      console.log("%s: sending %s to %s", _txn_id, txn.amount_wei, txn.to);
+      payable(txn.to).transfer(txn.amount_wei);
+    }
+
+    receive() external payable {}
 }
--- test/multisig_wallet.ts
+++ test/multisig_wallet.ts
@@ -14,6 +14,8 @@ describe("MultisigWallet", function() {
       signer1,
       signer2,
       signer3,
+      signer4,
+      payee,
       bad_signer
     ] = await ethers.getSigners();
 
@@ -22,7 +24,16 @@ describe("MultisigWallet", function() {
     );
     const multisig_wallet = await MultisigWalletFactory.deploy(2, 3);
 
-    return { multisig_wallet, owner, signer1, signer2, signer3, bad_signer };
+    return {
+      multisig_wallet,
+      owner,
+      signer1,
+      signer2,
+      signer3,
+      signer4,
+      payee,
+      bad_signer
+    };
   }
 
   describe("Deployment", function() {
@@ -31,14 +42,8 @@ describe("MultisigWallet", function() {
       const MultisigWalletFactory = await ethers.getContractFactory(
         "MultisigWallet"
       );
-      let has_errored = false;
-      try {
-        await MultisigWalletFactory.deploy(3, 2);
-      } catch (error) {
-        has_errored = true;
-        expect(error.method).to.equal("estimateGas");
-      }
-      expect(has_errored).to.equal(true);
+      // dont need to await if we expect to revert
+      expect(MultisigWalletFactory.deploy(3, 2)).to.be.reverted;
     });
 
     it("Should set the correct owner of this contract", async function() {
@@ -54,6 +59,114 @@ describe("MultisigWallet", function() {
       );
       expect(await multisig_wallet.signers_required()).to.equal(2);
       expect(await multisig_wallet.max_wallets()).to.equal(3);
+    });
+  });
+
+  describe("Core", async () => {
+    it("Should only allow owner to add signer wallets", async () => {
+      const { multisig_wallet, bad_signer } = await loadFixture(
+        deployMultisigWalletTwoOfThreeFixture
+      );
+      expect(
+        multisig_wallet.connect(bad_signer).addSigner(bad_signer.address)
+      ).to.be.revertedWith("Only the owner can access this function.");
+    });
+
+    it("Should add signer if max number of valid wallets not met", async () => {
+      const {
+        multisig_wallet,
+        owner,
+        signer1,
+        signer2,
+        signer3,
+        signer4,
+        bad_signer
+      } = await loadFixture(deployMultisigWalletTwoOfThreeFixture);
+      await multisig_wallet.addSigner(signer1.address);
+      await multisig_wallet.addSigner(signer2.address);
+      await multisig_wallet.addSigner(signer3.address);
+      expect(multisig_wallet.addSigner(signer4.address)).to.be.revertedWith(
+        "Adding another wallet will exceed max wallet amount."
+      );
+    });
+
+    it("Should not add signer if wallet is already a signer", async () => {
+      const { multisig_wallet, signer1 } = await loadFixture(
+        deployMultisigWalletTwoOfThreeFixture
+      );
+
+      await multisig_wallet.addSigner(signer1.address);
+      expect(multisig_wallet.addSigner(signer1.address)).to.be.revertedWith(
+        "This address is already a valid signer!"
+      );
+    });
+
+    it("Should create the transaction and fund the account", async () => {
+      const { multisig_wallet, signer1, payee, owner } = await loadFixture(
+        deployMultisigWalletTwoOfThreeFixture
+      );
+      await multisig_wallet.addSigner(signer1.address);
+      // await multisig_wallet.connect(signer1);
+
+      expect(
+        multisig_wallet.connect(signer1).createTransaction(payee, 500000000000)
+      ).to.be.revertedWith(
+        "Not enough Ether in this account to create transaction."
+      );
+
+      // fund the contract with the owners money
+      let ether = "1000000000000000000";
+
+      let transaction = {
+        to: multisig_wallet.address,
+        value: ether
+      };
+
+      await owner.sendTransaction(transaction);
+
+      expect(
+        await ethers.provider.getBalance(multisig_wallet.address)
+      ).to.equal(ether);
+
+      await multisig_wallet
+        .connect(signer1)
+        .createTransaction(payee.address, 500000000000);
+
+      // will fail if index does not exist
+      await multisig_wallet.pendingTransactions(0);
+    });
+
+    it("Should only allow valid signer to sign transaction and send", async () => {
+      const {
+        multisig_wallet,
+        owner,
+        signer1,
+        signer2,
+        payee,
+        bad_signer
+      } = await loadFixture(deployMultisigWalletTwoOfThreeFixture);
+
+      await multisig_wallet.addSigner(signer1.address);
+      await multisig_wallet.addSigner(signer2.address);
+      let gasFees: number = 0;
+
+      // fund the contract with the owners money
+      let ether = "1000000000000000000";
+      let transaction = {
+        to: multisig_wallet.address,
+        value: ether
+      };
+      await owner.sendTransaction(transaction);
+      let txn_amount: number = 500000000000;
+
+      await multisig_wallet
+        .connect(signer1)
+        .createTransaction(payee.address, txn_amount);
+
+      await multisig_wallet.connect(signer1).signTransaction(0);
+
+      // signTxn with another guy and trigger the send
+      let txn2 = await multisig_wallet.connect(signer2).signTransaction(0);
     });
   });
 });
